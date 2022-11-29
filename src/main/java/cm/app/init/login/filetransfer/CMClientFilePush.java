d4af74c48b41fd0566c00485276f49bac37427d1
Mingyu Lim <mlim@konkuk.ac.kr>
2022-11-29 20:53:27 +0900

    updated javadoc comments of CMClientFilePush.

--- src/main/java/cm/app/init/login/filetransfer/CMClientFilePush.java
+++ src/main/java/cm/app/init/login/filetransfer/CMClientFilePush.java
@@ -17,14 +17,15 @@ import java.util.Scanner;
  * the cm-server.conf file.
  * <br>In cm-server.conf and cm-client.conf, enable the PERMIT_FILE_TRANSFER field by setting to 1 for
  * the simplicity of the file transfer management of CM.
- * <br>After you run this client, you can also run another receiver client (CMClientFileReceiver)
- * if you want to send files to the client.
+ * <br>After you run this client, you can also run another receiver client (CMClientFileReceiver) at
+ * a different device if you want to send files to the client.
  * <br>1. The client logs in to the server with the user name "ccslab".
  * <br>2. If you press the enter key, the client shows a file chooser where you can select files.
  * <br>3. If you click confirm button, you can type a receiver: "SERVER" for the server or "mlim"
  * for the receiver client.
  * <br>4. If you press the enter key, CM and the client terminates.
- */public class CMClientFilePush {
+ */
+public class CMClientFilePush {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         CMClientApp client = new CMClientApp();
